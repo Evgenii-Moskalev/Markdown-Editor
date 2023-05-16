cca4e492e89395737d700eb23f9aeab158464750
Evgenii Moskalev <evgenii.moskalev.cohort2211@gmail.com>
2023-05-16 00:23:18 -0700

    Feat: Put the most recently-modified note at the top of notes

--- src/App.js
+++ src/App.js
@@ -29,11 +29,19 @@ export default function App() {
   }
 
   function updateNote(text) {
-    setNotes(oldNotes => oldNotes.map(oldNote => {
-      return oldNote.id === currentNoteId
-        ? { ...oldNote, body: text }
-        : oldNote
-    }))
+    // Put the most recently-modified note at the top
+    setNotes(oldNotes => {
+      const newArray = [];
+      for (let i = 0; i < oldNotes.length; i++) {
+        // const oldNote = oldNotes[i];
+        if (oldNotes[i].id === currentNoteId) {
+          newArray.unshift({ ...oldNotes[i], body: text })
+        } else {
+          newArray.push(oldNotes[i])
+        }
+      }
+      return newArray
+    })
   }
 
   function findCurrentNote() {
